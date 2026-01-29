a6365098151e635e365ca94b7ecca7028f3c7752
Yuvaraja1717 <yuvarajrsyuvi01@gmail.com>
2026-01-29 23:36:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// Smooth scrolling for nav links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
